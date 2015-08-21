9c37243c4c40d28f65990c29c635e8f2734961e9
yin-coder <172569856@qq.com>
2015-08-21 21:19:45 +0800

    hash

---
+++ goNet/hash/GenX509Cert.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"crypto/rand"
+	"crypto/rsa"
+	"crypto/x509"
+	"crypto/x509/pkix"
+	"encoding/gob"
+	"encoding/pem"
+	"fmt"
+	"math/big"
+	"os"
+	"time"
+)
+
+func main() {
+	random := rand.Reader
+	var key rsa.PrivateKey
+	loadKey("private.key", &key)
+
+	now := time.Now()
+	then := now.Add(60 * 60 * 24 * 365 * 1000 * 1000 * 1000)
+
+}
